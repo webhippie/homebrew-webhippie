5b7b9488543adbfaf216a8020e522940a6b99298
Thomas Boerger <thomas@webhippie.de>
2024-07-24 12:47:08 +0200

    ci: add flake config and automerge/update workflows

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Nix flake for development";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    devenv = {
+      url = "github:cachix/devenv";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        imports = [
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        ];
+
+        devenv = {
+          shells = {
+            default = {
+              languages = {
+                ruby = {
+                  enable = true;
+                  package = pkgs.ruby_3_3;
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+}
